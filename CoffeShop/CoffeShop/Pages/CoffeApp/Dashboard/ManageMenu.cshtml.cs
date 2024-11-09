a5ac3e77a62361c1829ad3c1ac5908288ff7a1f3
tungnm <kakal0tdz04@gmail.com>
2024-11-09 21:10:44 +0700

    mangemenu

---
+++ CoffeShop/CoffeShop/Pages/CoffeApp/Dashboard/ManageMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoffeShop.Pages.CoffeApp.Dashboard
+{
+    public class ManageMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
